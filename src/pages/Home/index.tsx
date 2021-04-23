b90bb251604f7c2b7b439b86a57fab2888d8b8c3
WeiJinjzH <991775996@qq.com>
2021-04-23 22:37:55 +0800

    新增 SelectSearch组件 下拉框支持搜索高亮

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,7 +5,7 @@ import {
 import { FormInstance } from 'antd/lib/form'
 import moment from 'moment'
 import React, { Component } from 'react'
-import BreadcrumbCamp from 'src/components/BreadcrumbCamp'
+import SelectSearch from 'src/components/SelectSearch'
 
 class Home extends Component<any, any> {
     form: FormInstance;
@@ -19,16 +19,18 @@ class Home extends Component<any, any> {
     constructor(props) {
         super(props)
         this.state = {
-            breadcrumbArr: [
-                { name: '一级菜单' },
-                [
-                    { name: '二级菜单1', path: '/echain-list' },
-                    { name: '二级菜单2' },
-                    { name: '二级菜单3' },
-                    { name: '二级菜单4' },
-                ],
-                { name: '三级菜单', path: '/echain-list' },
-                { name: '当前菜单' },
+            optionArr: [
+                { customerName: '周杰伦', animalName: '旺财', customerPhone: '15573220014', id: 0 },
+                { customerName: '林俊杰', animalName: '旺财旺旺', customerPhone: '15500080014', id: 1 },
+                { customerName: '冯绍峰', animalName: '旺财来了', customerPhone: '18500080099', id: 3 },
+                { customerName: '蔡依林', animalName: '大黑', customerPhone: '18398650909', id: 4 },
+                { customerName: '王心凌', animalName: '我家旺财', customerPhone: '13481095658', id: 5 },
+                { customerName: '赵丽颖', animalName: '财宝', customerPhone: '13481095658', id: 6 },
+                { customerName: '林心如', animalName: '啦啦啦', customerPhone: '19765908790', id: 7 },
+                { customerName: '吴亦凡', animalName: '大碗宽面', customerPhone: '19765908790', id: 8 },
+                { customerName: '田馥甄', animalName: '旺旺', customerPhone: '15590876098', id: 9 },
+                { customerName: '单依纯', animalName: '就是旺', customerPhone: '1830976912', id: 10 },
+                { customerName: '张韶涵', animalName: '破茧', customerPhone: '18309098790', id: 11 },
             ]
         }
         this.hidden = false
@@ -37,7 +39,7 @@ class Home extends Component<any, any> {
     render() {
         return (
             <div>
-                <BreadcrumbCamp breadcrumbArr={this.state.breadcrumbArr} />
+                <SelectSearch optionArr={this.state.optionArr} />
             </div>
         )
     }
