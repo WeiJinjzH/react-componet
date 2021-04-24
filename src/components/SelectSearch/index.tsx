b92a7bd0401a918e7572b1aae22d5e493dc72376
WeiJinjzH <991775996@qq.com>
2021-04-24 13:43:12 +0800

    SelectSearch组件支持搜索高亮

--- src/components/SelectSearch/index.tsx
+++ src/components/SelectSearch/index.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import { Select } from 'antd'
-import {
+import React, {
     useEffect, useState, useRef,
 } from 'react'
+import {Select} from 'antd'
 
-const SelectSearch = (props) => {
-    const { optionArr, currentValue='' } = props
-    const { Option } = Select
+export interface IProps {
+    optionArr: Array<{
+        customerName: string,
+        animalName: string,
+        customerPhone: string,
+        id: number | string
+    }>;
+    currentValue?: string
+}
+
+const SelectSearch = (props: IProps) => {
+    const {optionArr, currentValue = ''} = props
+    const {Option} = Select
+    const [initialData, updateData] = useState(optionArr)
     const [searchValue, setSearchValue] = useState(currentValue)
 
     const onChange = (value) => {
@@ -14,24 +24,23 @@ const SelectSearch = (props) => {
     }
 
     const onSearch = (value) => {
-
+        console.log(value)
+        setSearchValue(value)
     }
 
-    const loop = data =>
-      data.map(item => {
-        const index = item.animalName.indexOf(searchValue);
-        const beforeStr = item.animalName.substr(0, index);
-        const afterStr = item.animalName.substr(index + searchValue?.length);
-        const animalName =
-            index > -1 ? (
-                <span>
+    const loop = (data) => data.map((item) => {
+        const index = item.animalName.indexOf(searchValue)
+        const beforeStr = item.animalName.substr(0, index)
+        const afterStr = item.animalName.substr(index + searchValue?.length)
+        const animalName = index > -1 ? (
+            <span>
                 {beforeStr}
-                <span style={{ color: 'red' }}>{searchValue}</span>
+                <span style={{color: 'red'}}>{searchValue}</span>
                 {afterStr}
-                </span>
-            ) : (
-                <span>{item.animalName}</span>
-            );
+            </span>
+        ) : (
+            <span>{item.animalName}</span>
+        )
 
         return {
             animalName,
@@ -42,20 +51,47 @@ const SelectSearch = (props) => {
     return (
         <Select
             showSearch
-            style={{ width: 200 }}
+            style={{width: 200}}
             onChange={onChange}
             onSearch={onSearch}
             defaultValue={0}
-            filterOption={(input, option) =>
-                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-            }
+            filterOption={(input, option) => {
+                const label = option?.children.map((span) => span.props.children).join('')
+                console.log(label)
+                if (label.toLowerCase().indexOf(input.toLowerCase()) > -1) {
+                    return true
+                }
+                return false
+            }}
         >
             {
-                optionArr.map((option, index) => (
-                    <Option key={`${option.customerName}-${index}`} value={option.id}>
-                        {`${option.animalName}-${option.customerName}-${option.customerPhone}`}
-                    </Option>
-                ))
+                initialData.map((option, index) => {
+                    const {animalName, customerName, customerPhone} = option
+                    const regexp = new RegExp(searchValue, 'gi')
+                    // let c = <span>{animalName}-{customerName}-{customerPhone}</span>
+                    // const i = c.props.children.join('').indexOf(searchValue)
+                    // const beforeStr = c.props.children.join('').substr(0, i)
+                    // const afterStr = c.props.children.join('').substr(i + searchValue?.length)
+                    // if (regexp) {
+                    //     c = (
+                    //     <span>
+                    //         {beforeStr}
+                    //         <span style={{ color: 'red' }}>{searchValue}</span>
+                    //         {afterStr}
+                    //     </span>
+                    //     )
+                    // }
+                    const c = `${animalName}-${customerName}-${customerPhone}`.split('').map((txt, index) => (
+                        <span
+                            key={`${txt}-${index}`}
+                            style={{ color: searchValue.indexOf(txt) > -1 ? 'red' : 'initial'}}
+                        >{txt}</span>
+                    ))
+
+                    return (
+                        <Option key={`${option.customerName}-${index}`} value={option.id} children={c}/>
+                    )
+                })
             }
         </Select>
     )
